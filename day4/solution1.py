df8096a0bfc6b5a0a3d54468531c1fd0c11144d4
spencrc <145073726+spencrc@users.noreply.github.com>
2025-12-04 01:53:25 -0500

    refactored day4 part 1

--- day4/solution1.py
+++ day4/solution1.py
@@ -5,97 +5,52 @@ def main():
     marked_count = 0
     with open(sys.argv[1]) as file:
         lines_list = file.read().splitlines()
+
         for i in range(len(lines_list)):
             cur_line = lines_list[i]
             output_line = list()
-            if i == 0:
-                next_line = lines_list[i+1]
-
-                for j in range(len(cur_line)):
-                    if cur_line[j] == ".":
+            for j in range(len(cur_line)):
+                if cur_line[j] == ".":
                         output_line.append(".")
                         continue
 
-                    count = 0
-                    if j != 0:
-                        if cur_line[j-1] == "@": # looking left OR down-left
-                            count += 1
-                        if next_line[j-1] == "@":
-                            count += 1
-                    if j != len(cur_line) - 1:
-                        if cur_line[j+1] == "@": # looking right OR down-right
-                            count += 1
-                        if next_line[j+1] == "@":
-                            count += 1
-                    if next_line[j] == "@": # looking down
+                count = 0
+                check_down = False
+                next_line = list()
+                if i != len(lines_list) - 1:
+                    check_down = True
+                    next_line = lines_list[i+1]
+
+                check_up = False
+                prev_line = list()
+                if i != 0:
+                    check_up = True
+                    prev_line = lines_list[i-1]
+
+                if j != 0:
+                    if cur_line[j-1] == "@":
                         count += 1
-
-                    if count < 4:
-                        output_line.append("x")
-                        marked_count += 1
-                    else:
-                        output_line.append("@")
-            elif i == len(lines_list) - 1:
-                prev_line = lines_list[i-1]
-
-                for j in range(len(cur_line)):
-                    if cur_line[j] == ".":
-                        output_line.append(".")
-                        continue
-
-                    count = 0
-                    if j != 0:
-                        if cur_line[j-1] == "@": # looking left OR up-left
-                            count += 1
-                        if prev_line[j-1] == "@":
-                            count += 1
-                    if j != len(cur_line) - 1:
-                        if cur_line[j+1] == "@": # looking right OR up-right
-                            count += 1
-                        if prev_line[j+1] == "@":
-                            count += 1
-                    if prev_line[j] == "@": # looking up
+                    if check_down and next_line[j-1] == "@":
                         count += 1
-
-                    if count < 4:
-                        output_line.append("x")
-                        marked_count += 1
-                    else:
-                        output_line.append("@")
-            else:
-                next_line = lines_list[i+1]
-                prev_line = lines_list[i-1]
-
-                for j in range(len(cur_line)):
-                    if cur_line[j] == ".":
-                        output_line.append(".")
-                        continue
-
-                    count = 0
-                    if j != 0:
-                        if cur_line[j-1] == "@":
-                            count += 1
-                        if next_line[j-1] == "@":
-                            count += 1
-                        if prev_line[j-1] == "@": 
-                            count += 1
-                    if j != len(cur_line) - 1:
-                        if cur_line[j+1] == "@":
-                            count += 1
-                        if next_line[j+1] == "@":
-                            count += 1
-                        if prev_line[j+1] == "@": 
-                            count += 1
-                    if next_line[j] == "@":
+                    if check_up and prev_line[j-1] == "@": 
                         count += 1
-                    if prev_line[j] == "@": # looking down OR up
+                if j != len(cur_line) - 1:
+                    if cur_line[j+1] == "@":
                         count += 1
+                    if check_down and next_line[j+1] == "@":
+                        count += 1
+                    if check_up and prev_line[j+1] == "@": 
+                        count += 1
+                if check_down and next_line[j] == "@":
+                    count += 1
+                if check_up and prev_line[j] == "@": # looking down OR up
+                    count += 1
 
-                    if count < 4:
-                        output_line.append("x")
-                        marked_count += 1
-                    else:
-                        output_line.append("@")
+                if count < 4:
+                    output_line.append("x")
+                    marked_count += 1
+                else:
+                    output_line.append("@")
             output.append("".join(output_line))
     print("\n".join(output))
     print("\nThe number of accessible rolls is:", marked_count)
